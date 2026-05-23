85885ff1044e56504acce4c9a4cd672b4d888fa1
redstone59 <redstonepro23@googlemail.com>
2026-05-23 18:58:13 +1000

    Add `GuessResult` struct and its respective enums

---
+++ src/guess.c
@@ -0,0 +1,28 @@
+#include <stdbool.h>
+
+typedef enum RelativeGuess {
+    RELATIVE_CORRECT,
+    RELATIVE_HIGHER,
+    RELATIVE_LOWER
+} RelativeGuess;
+
+typedef struct GuessResult {
+    bool primaryColourCorrect;
+    bool secondaryColourCorrect;
+
+    RelativeGuess difficulty;
+    RelativeGuess length;
+    RelativeGuess downloads;
+    RelativeGuess likes;
+    RelativeGuess objectCount;
+} GuessResult;
+
+bool GuessResult_IsCorrect(GuessResult guess) {
+    return guess.primaryColourCorrect &&
+           guess.secondaryColourCorrect &&
+           guess.difficulty == RELATIVE_CORRECT &&
+           guess.length == RELATIVE_CORRECT &&
+           guess.downloads == RELATIVE_CORRECT &&
+           guess.likes == RELATIVE_CORRECT &&
+           guess.objectCount == RELATIVE_CORRECT;
+}
